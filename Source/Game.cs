bf4e2ff13f4df39ef61b6f620768e3c1ff5945bf
Samuel M <76839403+Syntrice@users.noreply.github.com>
2024-11-16 11:10:27 +0000

    Add test code to Game.cs for displaying a grid of cells
    Generate and display a grid of random black and white tiles. This uses the monogame extended library to render
    rectangles to a resizable, scaling viewport.

--- Source/Game.cs
+++ Source/Game.cs
@@ -1,11 +1,23 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended.Graphics;
+using MonoGame.Extended;
+using System;
+using MonoGame.Extended.ViewportAdapters;
 
 namespace MonoASCII
 {
     public class Game : Microsoft.Xna.Framework.Game
     {
+        public const int TileSize = 10;
+        public const int ViewportWidth = 32;
+        public const int ViewportHeight = 32;
+
+        private OrthographicCamera _camera;
+        private int[,] _tile_map;
+        private Random _random;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
@@ -14,28 +26,53 @@ namespace MonoASCII
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+            _random = new Random();
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
+            SetupViewport();
+            GenerateMap();
+        }
+
+        private void SetupViewport()
+        {
+            var viewportAdapter = new BoxingViewportAdapter(Window, GraphicsDevice, ViewportWidth, ViewportHeight);
+            _camera = new OrthographicCamera(viewportAdapter);
+            viewportAdapter.Reset();
+            Window.AllowUserResizing = true;
+        }
+
+        private void GenerateMap()
+        {
+            _tile_map = new int[ViewportWidth, ViewportHeight];
+            for (int i = 0; i < ViewportWidth; i++)
+            {
+                for (int j = 0; j < ViewportHeight; j++)
+                {
+                    if (_random.NextDouble() < 0.5)
+                    {
+                        _tile_map[i, j] = 0;
+                    }
+                    else
+                    {
+                        _tile_map[i, j] = 1;
+                    }
+                }
+            }
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-
-            // TODO: Add your update logic here
 
             base.Update(gameTime);
         }
@@ -44,9 +81,24 @@ namespace MonoASCII
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
-
             base.Draw(gameTime);
+
+            _spriteBatch.Begin(transformMatrix: _camera.GetViewMatrix(), samplerState: SamplerState.PointClamp);
+            for (int i = 0; i < ViewportWidth; i++)
+            {
+                for (int j = 0; j < ViewportHeight; j++)
+                {
+                    if (_tile_map[i, j] == 0)
+                    {
+                        _spriteBatch.DrawRectangle(new Rectangle(i, j, 1, 1), Color.White);
+                    }
+                    else if (_tile_map[i, j] == 1)
+                    {
+                        _spriteBatch.DrawRectangle(new Rectangle(i, j, 1, 1), Color.Black);
+                    }
+                }
+            }
+            _spriteBatch.End();
         }
     }
 }
